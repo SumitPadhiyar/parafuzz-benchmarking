64a249ecc7a334c866f1e2ff15ed0fbbc19f12fa
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-04-23 19:54:48 +0530

    Added RAX benchmark from jpf

---
+++ RAX-jpf/run.sh
@@ -0,0 +1,1 @@
+AFL_NO_FORKSRV=1 /home/skip/Downloads/softwares/AFL/afl-fuzz -m none -i ip -o op/ ./a.out @@
---
+++ RAX-jpf/test.ml
@@ -0,0 +1,35 @@
+module Crowbar = Parafuzz_lib.Crowbar
+
+module Event = struct
+    open Domain
+
+(*     type t = { count: int Atomic.t; mutex: Mutex.t; cv: Condition.t } *)
+    type t = { mutex: Mutex.t; cv: Condition.t }
+
+(*     let create () = {count = Atomic.make 0; mutex = Mutex.create (); cv = Condition.create ()} *)
+    let create () = { mutex = Mutex.create (); cv = Condition.create ()}
+
+(*     let get_count {count; _} = Atomic.get count *)
+
+    let wait_for_event {mutex; cv} = Mutex.lock mutex; Condition.wait cv mutex; Mutex.unlock mutex
+
+    let signal_event {mutex; cv}= 
+(*         Atomic.incr count;  *)
+        Mutex.lock mutex; 
+        Condition.broadcast cv;
+        Mutex.unlock mutex
+end
+
+let executive event = function () -> Event.signal_event event
+
+let test i = function () -> 
+    let event = Event.create () in
+    let d1 = Domain.spawn @@ executive event in
+    if(i = 100) then Event.wait_for_event event;
+    Domain.join d1
+
+let ()  = 
+	Crowbar.(add_test ~name:"Deadlock test" [Crowbar.int] (fun i ->
+		Parafuzz_lib.run @@ test i
+	))
+
