195365b436230a290ce06b9d252c21312d4c40e4
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-05-17 12:54:49 +0530

    Modified mysql-extract-maple

---
+++ mysql_extract_bug_maple/README.md
@@ -0,0 +1,5 @@
+# Mysql-extract
+Benchmark from maple paper. [source](https://github.com/jieyu/maple/blob/master/example/mysql_169_extract/main.cc)
+
+## Improvements
+1. None
--- mysql_extract_bug_maple/test.ml
+++ mysql_extract_bug_maple/test.ml
@@ -3,29 +3,23 @@ module Crowbar = Parafuzz_lib.Crowbar
 module Mysql_log = struct
     open Domain
 
-    type t = { mutable contents: string List.t; mutable reversed : bool;mutex: Mutex.t}
+    type t = { mutable contents: string List.t; mutex: Mutex.t}
 
-    let create () = { contents = []; reversed = false; mutex = Mutex.create ();}
+    let create () = { contents = []; mutex = Mutex.create ();}
 
     let write log content = 
         Mutex.lock log.mutex;
-        log.reversed <- false;
         log.contents <- content :: log.contents; 
         Mutex.unlock log.mutex
 
-    let get_head log = 
+    let get_tail log = 
         Mutex.lock log.mutex; 
-        if not log.reversed then (
-            log.contents <- List.rev log.contents;
-            log.reversed <- true;
-        );
         let v = List.hd log.contents in
         Mutex.unlock log.mutex;
         v
 
     let reset log = 
         Mutex.lock log.mutex; 
-        log.reversed <- false;
         log.contents <- [];
         Mutex.unlock log.mutex; 
 
@@ -61,22 +55,21 @@ let insert tab log =
     Mysql_table.insert_entry tab 1 2;
     Mysql_log.write log "insert"
 
-let test tab log = 
-    let d1 = Domain.spawn (fun () -> delete tab log) in
-    let d2 = Domain.spawn (fun () -> insert tab log) in
-
+let test tab log i = 
+    let d1 = Domain.spawn (fun () -> insert tab log) in
+    if i = 130 then  
+        delete tab log;
     Domain.join d1;
-    Domain.join d2;
 
-    if Mysql_table.is_empty tab then Crowbar.check @@ not (Mysql_log.get_head log = "remove")
-    else Crowbar.check @@ not (Mysql_log.get_head log = "insert")
+    if Mysql_table.is_empty tab then Crowbar.check @@ (Mysql_log.get_tail log = "remove")
+    else Crowbar.check @@ (Mysql_log.get_tail log = "insert")
 
 let ()  =
     let tab = Mysql_table.create () in
     let log = Mysql_log.create () in
-	Crowbar.(add_test ~name:"Mysql extract bug maple" [Crowbar.const 1] (fun _ ->
+	Crowbar.(add_test ~name:"Mysql extract bug maple" [Crowbar.int] (fun i ->
         Parafuzz_lib.run (fun () -> 
-            test tab log;
+            test tab log i;
             Mysql_table.remove_entries tab;
             Mysql_log.reset log
         );
