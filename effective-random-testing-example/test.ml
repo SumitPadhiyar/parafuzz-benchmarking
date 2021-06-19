e75441ef9bc4d56859779642a7abe3d084de1915
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-06-19 18:10:46 +0530

    Modified effective-random-testing-example example

--- effective-random-testing-example/test.ml
+++ effective-random-testing-example/test.ml
@@ -5,12 +5,12 @@ let rec write f i max =
     else ()
 
 let test () = 
-    let max = 30 in
+    let max = 20 in
     let x = Atomic.make 0 in
     let y = Atomic.make 0 in
     let d1 = Domain.spawn(fun () -> write (Atomic.set x) 1 max) in
     let d2 = Domain.spawn(fun () -> write (Atomic.set y) 1 max) in
-    Crowbar.check @@ (Atomic.get x <> (max-1)  && Atomic.get y <> (max-1));
+    Crowbar.check @@ (Atomic.get x <> (max-1) || Atomic.get y <> (max-1));
     Domain.join d1;
     Domain.join d2
 
