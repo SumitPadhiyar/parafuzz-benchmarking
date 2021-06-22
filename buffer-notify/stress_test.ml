2d142774984ea4e181d8aa9083c0f905785b95d4
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-06-22 17:00:10 +0530

    Added stress test and result in buffer-notify

---
+++ buffer-notify/stress_test.ml
@@ -0,0 +1,73 @@
+open Domain
+
+let items_produced = 2
+
+module Buffer = struct
+
+    let bufsize = ref 0
+
+    type t = {mutable first : int; mutable last : int; arr : int Array.t; lock: Mutex.t; cv: Condition.t}
+    
+    let create size = 
+        bufsize := size;
+        { first = 0; last = 0; arr = Array.make !bufsize 0; lock = Mutex.create (); cv = Condition.create ()}
+
+    let enq buf x =
+        Mutex.lock buf.lock;
+        while ((buf.last+1) mod !bufsize) = buf.first do
+            Condition.wait buf.cv buf.lock
+        done;
+        buf.arr.(buf.last) <- x;
+        buf.last <- (buf.last + 1) mod !bufsize;
+        Condition.signal buf.cv;
+        Mutex.unlock buf.lock
+
+    let deq buf =
+        Mutex.lock buf.lock;
+        while buf.first = buf.last do
+            Condition.wait buf.cv buf.lock
+        done;
+        let v = buf.arr.(buf.first) in
+        buf.first <- (buf.first + 1) mod !bufsize;
+        Condition.signal buf.cv;
+        Mutex.unlock buf.lock;
+        v
+
+end
+
+    
+let producer buf name = 
+    let rec loop i = 
+        if i < items_produced then (
+            Buffer.enq buf name;
+            loop (i+1)
+        ) else ()
+    in
+    loop 0
+
+
+let consumer buf =
+    let rec loop i = 
+        if i < (items_produced * 2) then (
+            ignore @@ Buffer.deq buf;
+            loop (i+1)
+        ) else ()
+    in
+    loop 0
+
+
+let test size = function () -> 
+    let buf = Buffer.create size in
+    let p1 = Domain.spawn (fun () -> producer buf 1) in
+    let p2 = Domain.spawn (fun () -> producer buf 2) in
+    let p3 = Domain.spawn (fun () -> consumer buf) in
+    Domain.join p1;
+    Domain.join p2;
+    Domain.join p3
+
+let ()  = 
+    Crowbar.(add_test ~name:"BufferNotify test" [Crowbar.int] (fun i ->
+       let size = if i = 130 then 2 else 3 in
+       test size ()
+	))
+
