951d96bcd24e49995b941160525e3becaafd1ea8
Sumit Padhiyar <padhiyarsumit@gmail.com>
2021-06-24 17:36:15 +0530

    Modified effective-random-testing

--- effective-random-testing-example/test.ml
+++ effective-random-testing-example/test.ml
@@ -4,18 +4,18 @@ let rec write f i max =
     if i <= max then (f i; write f (i+1) max)
     else ()
 
-let test () = 
+let test i = function () ->
     let max = 20 in
     let x = Atomic.make 0 in
     let y = Atomic.make 0 in
     let d1 = Domain.spawn(fun () -> write (Atomic.set x) 1 max) in
     let d2 = Domain.spawn(fun () -> write (Atomic.set y) 1 max) in
-    Crowbar.check @@ (Atomic.get x <> (max-1) || Atomic.get y <> (max-1));
+    Crowbar.check @@ (i <> 100 || (Atomic.get x <> (max-1) || Atomic.get y <> (max-1)));
     Domain.join d1;
     Domain.join d2
 
 
 let ()  = 
-	Crowbar.(add_test ~name:"Basic interleaving test" [Crowbar.const 1] (fun _ ->
-		Parafuzz_lib.run test
+	Crowbar.(add_test ~name:"Basic interleaving test" [Crowbar.int8] (fun i ->
+		Parafuzz_lib.run @@ test i
 	))
